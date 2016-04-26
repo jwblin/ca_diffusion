6005abe970a4eaf3fb1b4b306a31f0ddaa6c6d91
charln2 <charln2@uw.edu>
2016-04-26 11:53:02 -0700

    Added Heading
    Making first commit to test github. Making sure I commit to master from
    my own fork instead of the prof's master branch.

--- Diffusion.py
+++ Diffusion.py
@@ -1,3 +1,11 @@
+''' Diffusion.py: Builds Module 10.2 (Diffusion) from S&S
+    Group Members:
+        Alex Ducken
+        Branden Livermore
+        Charlie Nguyen
+        Tyler McBride
+'''
+
 __author__ = 'v-caearl'
 
 def diffusion(diffusionRate, site, N, NE, E, SE, S, SW, W, NW):
